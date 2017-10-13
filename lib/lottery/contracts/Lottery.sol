507a9efd51b72d47fafa44536cf176a242e9c568
Lior Saar <liorsaar1@gmail.com>
2017-10-13 08:23:32 -0700

    enter value

--- lib/lottery/contracts/Lottery.sol
+++ lib/lottery/contracts/Lottery.sol
@@ -1,27 +1,25 @@
 //pragma solidity ^0.4.8;
 
 contract Lottery {
-    address[] public entries;
-    /*uint public numEntries;*/
-    uint public totalValue;
+  address[] public entries;
+  uint public totalValue;
 
-    function Lottery(uint _totalValue) {
-        totalValue = _totalValue;
-        /*numEntries = 0;*/
-    }
+  function Lottery(uint _totalValue) {
+    totalValue = _totalValue;
+  }
 
-    function enter() returns (bool) {
-      // check capacity
-      if (entries.length >= totalValue) {
-        return false;
-      }
-      // enter the lottery
-      entries.push(msg.sender);
-      return true;
-        /*if(this.balance > totalValue) {
-            payout();
-        }*/
+  function enter() payable returns (bool) {
+    // check capacity
+    if (entries.length >= totalValue) {
+      return false;
     }
+    // enter the lottery
+    entries.push(msg.sender);
+    return true;
+      /*if(this.balance > totalValue) {
+          payout();
+      }*/
+   }
 /*
     function payout() internal {
         uint winner = uint(keccak256(entries)) % numEntries;
