6e4dd307f2f41336a77cd295bf713ef0ec7d8590
Lior Saar <liorsaar1@gmail.com>
2017-10-13 10:37:32 -0700

    cheap ticket

--- lib/lottery/contracts/Lottery.sol
+++ lib/lottery/contracts/Lottery.sol
@@ -3,14 +3,26 @@
 contract Lottery {
   address[] public entries;
   uint public totalValue;
+  uint public maxEntries;
+  uint public ticketPrice = 1 ether;
 
   function Lottery(uint _totalValue) {
+    // value must be higher than ticket price
+    if (_totalValue < ticketPrice) {
+      throw;
+    }
+    // all good
     totalValue = _totalValue;
+    maxEntries = totalValue / ticketPrice;
   }
 
   function enter() payable returns (bool) {
+    // check if ticket price satisfied
+    if (msg.value < ticketPrice) {
+      return false;
+    }
     // check capacity
-    if (entries.length >= totalValue) {
+    if (entries.length >= maxEntries) {
       return false;
     }
     // enter the lottery
